524ed6c040e1c9718908d6779e4caf4751ded71e
rvohmin <rvohmin@iflex.ru>
2021-03-14 13:58:34 +0300

    Исправлен подсчет выводимого количества

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -22,17 +22,21 @@ public class CarController {
         Car car3 = new Car("Opel", "333", "Blue");
         Car car4 = new Car("Niccan", "444", "Red");
         Car car5 = new Car("Volvo", "555", "Green");
+        Car car6 = new Car("Niccan", "567", "Yellow");
+        Car car7 = new Car("Volvo", "777", "Orange");
         cars.add(car1);
         cars.add(car2);
         cars.add(car3);
         cars.add(car4);
         cars.add(car5);
+        cars.add(car6);
+        cars.add(car7);
     }
 
     @GetMapping
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
         List<Car> collect;
-        if (count != null) {
+        if (count != null && count < 5) {
             collect = cars.stream().limit(count).collect(Collectors.toList());
         } else {
             collect = cars;
