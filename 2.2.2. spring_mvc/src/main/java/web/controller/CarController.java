e13b21cd02e2860356a4d3d5bf8f4bfb80682121
rvohmin <rvohmin@iflex.ru>
2021-03-14 13:40:37 +0300

    Готовый вариант

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,43 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+    List<Car> cars;
+    {
+        cars = new ArrayList<Car>();
+        Car car1 = new Car("Kia", "111", "Black");
+        Car car2 = new Car("BMW", "222", "White");
+        Car car3 = new Car("Opel", "333", "Blue");
+        Car car4 = new Car("Niccan", "444", "Red");
+        Car car5 = new Car("Volvo", "555", "Green");
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+    }
+
+    @GetMapping
+    public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
+        List<Car> collect;
+        if (count != null) {
+            collect = cars.stream().limit(count).collect(Collectors.toList());
+        } else {
+            collect = cars;
+        }
+        model.addAttribute("cars", collect);
+        return "car";
+    }
+}
---
+++ 2.2.2. spring_mvc/src/main/java/web/model/Car.java
@@ -0,0 +1,60 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars_mvc")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "series")
+    private String series;
+    @Column(name = "color")
+    private String color;
+
+    public Car() {
+    }
+
+    public Car(String model, String series, String color) {
+        this.model = model;
+        this.series = series;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series='" + series + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
